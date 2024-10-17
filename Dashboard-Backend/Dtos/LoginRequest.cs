08a229264f8a5ef5fdcfca3f23d9e071ad8c5027
SofiaSJ09 <sjimenezsofia@gmail.com>
2024-10-16 20:24:32 -0600

    feat: JWT

---
+++ Dashboard-Backend/Dtos/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dashboard_Backend.Dtos
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "El nombre de usuario es requerido")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        public string Password { get; set; }
+    }
+}
---
+++ Dashboard-Backend/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Dashboard_Backend.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+
+        // Simulación de una base de datos en memoria
+        private static List<User> Users = new List<User>();
+
+    }
+
+
+
+}
